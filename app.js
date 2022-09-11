4f73b7717b8b533d4210890fb2fedd29ceca0169
Larry Bezrkov <larry9498@gmail.com>
2022-09-11 04:52:04 +0300

    Add port to .env, fix the page refresh issue

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 /* eslint-disable no-console */
-// eslint-disable-next-line import/no-extraneous-dependencies, @typescript-eslint/no-var-requires
 const express = require('express');
+const history = require('express-history-api-fallback');
+require('dotenv').config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(`${__dirname}/dist`));
+app.use(history('index.html', { root: './dist' }));
 
 app.listen(PORT, () => {
-	console.log(`Example app listening on port ${PORT}!`);
+	console.log(`Listening on port ${PORT}!`);
 });
